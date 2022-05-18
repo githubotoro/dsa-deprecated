e8e463b4dcb9e13a2d5dc3f28d3bce7c97ea3d31
githubotoro <yupuday.git@gmail.com>
2022-05-18 16:44:49 +0530

    min-stack

--- stack/min-stack-extra-space.cpp
+++ stack/min-stack-extra-space.cpp
@@ -22,47 +22,47 @@ stack <int> ss;
 // then we will push in supporting stack
 // If supporting stack is empty, then again we will push in supporting stack
 void push(int ele) {
-    s.push(ele);
+	s.push(ele);
 
-    if(ss.size() == 0)
-        ss.push(ele);
-    else if(ss.top() >= ele)
-        ss.push(ele);
+	if(ss.size() == 0)
+		ss.push(ele);
+	else if(ss.top() >= ele)
+		ss.push(ele);
 }
 
 // If top of supporting stack is same as top of main stack,
 // then we will pop from both stacks
 // else we will pop only from main stack
 void pop() {
-    if(s.size() !=0 && ss.size() != 0) {
-        if(ss.top() == s.top()) ss.pop();
-        s.pop();
-    }
-    
+	if(s.size() !=0 && ss.size() != 0) {
+		if(ss.top() == s.top()) ss.pop();
+		s.pop();
+	}
+	
 }
 
 // If supporting stack is empty, then minimum is -1
 // else top of supporting stack will be minimum element
 int getMin() {
-    if(ss.size() == 0) return -1;
-    else return ss.top();
+	if(ss.size() == 0) return -1;
+	else return ss.top();
 }
 
 int main() {
 	stayTORO();
 
 	push(5);
-    push(15);
-    push(10);
-    push(0);
+	push(15);
+	push(10);
+	push(0);
 
-    cout<<"Min is: "<<getMin();
+	cout<<"Min is: "<<getMin();
 
-    pop();
+	pop();
 
-    s.push(10);
+	s.push(10);
 
-    cout<<"\nMin is: "<<getMin();
+	cout<<"\nMin is: "<<getMin();
 
 	return 0;
 }
--- stack/min-stack-no-extra-space.cpp
+++ stack/min-stack-no-extra-space.cpp
@@ -23,49 +23,49 @@ int minEle = -1;
 // and update the minimum element
 // else simply push element in the stack
 void push(int ele) {
-    if(s.size() == 0) {
-        s.push(ele);
-        minEle = ele;
-    } else {
-        if(ele < minEle) {
-            s.push(2 * ele - minEle);
-            minEle = ele;
-        } else s.push(ele);
-    }
+	if(s.size() == 0) {
+		s.push(ele);
+		minEle = ele;
+	} else {
+		if(ele < minEle) {
+			s.push(2 * ele - minEle);
+			minEle = ele;
+		} else s.push(ele);
+	}
 }
 
 // If top of stack is less than minimum element,
 // then minimum element will be: 2 * minimum element - top of stack
 void pop() {
-    if(s.size() != 0) {
-        if(s.top() < minEle)
-            minEle = 2 * minEle - s.top();
-        
-        s.pop();
-    }
+	if(s.size() != 0) {
+		if(s.top() < minEle)
+			minEle = 2 * minEle - s.top();
+		
+		s.pop();
+	}
 }
 
 // If stack is empty, then minimum is -1
 // else there would be another value in minimum element
 int getMin() {
-    return minEle;
+	return minEle;
 }
 
 int main() {
 	stayTORO();
 
 	push(5);
-    push(15);
-    push(10);
-    push(0);
+	push(15);
+	push(10);
+	push(0);
 
-    cout<<"Min is: "<<getMin();
+	cout<<"Min is: "<<getMin();
 
-    pop();
+	pop();
 
-    s.push(10);
+	s.push(10);
 
-    cout<<"\nMin is: "<<getMin();
+	cout<<"\nMin is: "<<getMin();
 
 	return 0;
 }
