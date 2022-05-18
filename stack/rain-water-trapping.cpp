5944c96b3d1e3e316c40705e28795355fe18faff
githubotoro <yupuday.git@gmail.com>
2022-05-18 13:12:09 +0530

    rain-water-trapping

--- stack/rain-water-trapping.cpp
+++ stack/rain-water-trapping.cpp
@@ -15,39 +15,39 @@ void stayTORO() {
 int main() {
 	stayTORO();
 
-    int n;
-    cin>>n;
+	int n;
+	cin>>n;
 
-    int arr[n];
+	int arr[n];
 
-    // Taking input array
-    for(auto &ele: arr) cin>>ele;
+	// Taking input array
+	for(auto &ele: arr) cin>>ele;
 
-    // Declaring arrays for max left, max right and water
-    int maxL[n], maxR[n], water[n];
+	// Declaring arrays for max left, max right and water
+	int maxL[n], maxR[n], water[n];
 
-    // We will initialize first element of max left
-    // and last element of max right
-    maxL[0] = arr[0];
-    maxR[n-1] = arr[n-1];
-    
-    // Max Left traverses from beginning
-    // Max Right traverses from ending
-    for(int i=1, j=n-2; i<n, j>=0; i++, j--) {
-        maxL[i] = max(arr[i], maxL[i-1]);
-        maxR[j] = max(arr[j], maxR[j+1]);
-    }
+	// We will initialize first element of max left
+	// and last element of max right
+	maxL[0] = arr[0];
+	maxR[n-1] = arr[n-1];
+	
+	// Max Left traverses from beginning
+	// Max Right traverses from ending
+	for(int i=1, j=n-2; i<n, j>=0; i++, j--) {
+		maxL[i] = max(arr[i], maxL[i-1]);
+		maxR[j] = max(arr[j], maxR[j+1]);
+	}
 
-    int totalWater = 0;
+	int totalWater = 0;
 
-    // Formula:
-    // water in ith building = minimum( maximum of left, maximum of right) - height of building
-    for(int i=0; i<n; i++) {
-        water[i] = min(maxL[i], maxR[i]) - arr[i];
-        totalWater += water[i];
-    }
+	// Formula:
+	// water in ith building = minimum( maximum of left, maximum of right) - height of building
+	for(int i=0; i<n; i++) {
+		water[i] = min(maxL[i], maxR[i]) - arr[i];
+		totalWater += water[i];
+	}
 
-    cout<<totalWater;
+	cout<<totalWater;
 
 	return 0;
 }
