c80a76a850baa8c8a56a167405821a2e153f3ceb
githubotoro <yupuday.git@gmail.com>
2022-05-21 10:43:49 +0530

    print-to-n

---
+++ recursion/print-1-to-n.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void stayTORO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif 
+}
+
+// Hypothesis:
+// print(n) will print 1 to n
+// Logic:
+// print(n-1) will print from 1 to n-1,
+// then I will print nth element
+void print(int n) {
+	// Base Condition
+	if(n==0) return;
+
+	// Hypothesis
+    print(n-1);
+
+	// Induction Step
+	cout<<n<<endl;
+}
+
+
+int main() {
+	stayTORO();
+
+	int n = 5;
+
+	print(n);
+
+	return 0;
+}
+
+// OUT
+// 1
+// 2
+// 3
+// 4
+// 5
---
+++ recursion/print-n-to-1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void stayTORO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif 
+}
+
+// Hypothesis:
+// print(n) will print n to 1
+// Logic:
+// I will print the nth element,
+// then print(n-1) will print from n-1 to 1
+void print(int n) {
+	// Base Condition
+	if(n==0) return;
+
+    // Induction Step
+    cout<<n<<endl;
+
+	// Hypothesis
+    print(n-1);
+}
+
+
+int main() {
+	stayTORO();
+
+	int n = 5;
+
+	print(n);
+
+	return 0;
+}
+
+// OUT
+// 5
+// 4
+// 3
+// 2
+// 1
