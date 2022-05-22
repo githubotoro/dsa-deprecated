d9303fdbaa1bfb032d32638e7f1f1b04d9142b17
githubotoro <yupuday.git@gmail.com>
2022-05-22 17:41:41 +0530

    n-bit-binary

--- recursion/n-bit-binary.cpp
+++ recursion/n-bit-binary.cpp
@@ -22,19 +22,19 @@ void stayTORO() {
 // We can put either "0" or "1"
 // Else, we have only 1 choice, put "1"
 void nBitBinary(string ans, int ones, int zeroes, int n) {
-    // Base Condition
-    if(n == 0) {
-        cout<<ans<<endl;
-        return;
-    }
+	// Base Condition
+	if(n == 0) {
+		cout<<ans<<endl;
+		return;
+	}
 
-    // Hypothesis and Induction
-    if(ones > zeroes) {
-        nBitBinary(ans+"1", ones+1, zeroes, n-1);
-        nBitBinary(ans+"0", ones, zeroes+1, n-1);
-    } else {
-        nBitBinary(ans+"1", ones+1, zeroes, n-1);
-    }
+	// Hypothesis and Induction
+	if(ones > zeroes) {
+		nBitBinary(ans+"1", ones+1, zeroes, n-1);
+		nBitBinary(ans+"0", ones, zeroes+1, n-1);
+	} else {
+		nBitBinary(ans+"1", ones+1, zeroes, n-1);
+	}
 }
 
 int main() {
