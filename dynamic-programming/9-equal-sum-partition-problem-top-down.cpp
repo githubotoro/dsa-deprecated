30ebb5c254c86a630d356c1ba6d21a5c3a83397c
githubotoro <yupuday.git@gmail.com>
2022-11-10 19:19:47 +0530

    equal-sum-partition-problem-top-down-update

--- dynamic-programming/9-equal-sum-partition-problem-top-down.cpp
+++ dynamic-programming/9-equal-sum-partition-problem-top-down.cpp
@@ -12,48 +12,41 @@ void stayTORO() {
 	#endif 
 }
 
-bool equalSumPartition(int arr[], int n, int sum1, int sum2) {
-    int maxSum = 0;
-    for(int i=0; i<n; i++) maxSum+=arr[i];
+bool subsetSum(int arr[], int n, int sum) {
+	int dp[n+1][sum+1];
+	memset(dp, -1, sizeof(dp));
 
-    int dp[n+1][maxSum+1][maxSum+1];
-    memset(dp, -1, sizeof(dp));
+	for(int i=0; i<=n; i++) dp[i][0] = true;
+	for(int j=1; j<=sum; j++) dp[0][j] = false;
 
-    dp[0][0][0] = 0;
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=sum; j++) {
+			if(arr[i-1] <= j) {
+				dp[i][j] = (dp[i-1][j-arr[i-1]] || dp[i-1][j]);
+			} else {
+				dp[i][j] = dp[i-1][j];
+			}
+		}
+	}
 
-    for(int j=1; j<=sum1; j++) {
-        dp[0][j][0] = false;
-    }
-
-    for(int k=1; k<=sum2; k++) {
-        dp[0][0][k] = false;
-    }
-
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=sum1; j++) {
-            for(int k=1; k<=sum2; k++) {
-                dp[i][j][k] = ( dp[i-1][j+arr[i-1]][k] || dp[i-1][j][k+arr[i-1]] );
-            }
-        }
-    }
-
-	for(int m=1; m<=maxSum; m++) {
-        if(dp[n][m][m] == 1) return true;
-    }
-
-    return false;
+	return dp[n][sum];
 }
 
 int main() {
 	stayTORO();
 
-	int arr[] = {1, 5, 24, 20};
+	int arr[] = {1, 5, 11, 5};
 	int n = sizeof(arr) / sizeof(arr[0]);
-	int sum1 = 0;
-	int sum2 = 0;
+
+	int maxSum = 0;
+	for(int i=0; i<n; i++) maxSum+=arr[i];
 	
-	bool partitionPossible = equalSumPartition(arr, n, sum1, sum2);
-	cout<<partitionPossible;
+	if(maxSum%2 == 1) cout<<false;
+	else {
+		int sum = maxSum/2;
+		bool partitionPossible = subsetSum(arr, n, sum);
+		cout<<partitionPossible;
+	}
 
 	return 0;
 }
