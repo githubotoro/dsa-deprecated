e9b06e9be087d16bb2440757385b6adca0874323
githubotoro <yupuday.git@gmail.com>
2022-11-10 15:51:30 +0530

    knapsack-memoization

--- dynamic-programming/2-knapsack-memoization.cpp
+++ dynamic-programming/2-knapsack-memoization.cpp
@@ -12,30 +12,31 @@ void stayTORO() {
 	#endif 
 }
 
-int dp[100][1000];
-memset(dp, -1, sizeof(dp));
+int dp[100][100];
 
 int knapsack(int wt[], int val[], int n, int W) {
 	// base condition
-	if(n==0 || W==0) return 0;
+	if(dp[n][W] != -1) return dp[n][W];
 
 	// hypothesis and induction
 	if(wt[n-1] <= W) {
 		// return maximum of (include, exclude)
-		return max(val[n-1] + knapsack(wt, val, n-1, W-wt[n-1]), knapsack(wt, val, n-1, W));
+		return dp[n][W] = max(val[n-1] + knapsack(wt, val, n-1, W-wt[n-1]), knapsack(wt, val, n-1, W));
 	} else {
 		// we cannot include the element, so only one case - exclude
-		return knapsack(wt, val, n-1, W);
+		return dp[n][W] = knapsack(wt, val, n-1, W);
 	}
 }
 
 int main() {
 	stayTORO();
 
-	int wt[] = {60, 100, 120};
-	int val[] = {10, 20, 30};
+	memset(dp, -1, sizeof(dp));
+
+	int wt[] = {1, 2, 4, 9, 10, 20};
+	int val[] = {4, 20, 8, 36, 20, 80};
 	int n = sizeof(wt) / sizeof(wt[0]);
-	int W = 500;
+	int W = 20;
 	
 	int maxProfit = knapsack(wt, val, n, W);
 	cout<<maxProfit;
