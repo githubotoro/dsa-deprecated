dd1c41fc79c09b0300f3e91db73f77ef66a7eec3
githubotoro <yupuday.git@gmail.com>
2022-11-14 20:26:51 +0530

    graphs-update

--- graphs/12-number-of-enclaves.cpp
+++ graphs/12-number-of-enclaves.cpp
@@ -12,28 +12,28 @@ void stayTORO() {
 	#endif 
 }
 
-// DFS - Depth First Search
-// Principle - LIFO (Last In First Out)
+// BFS - Breadth First Search
+// Principle - FIFO (First In First Out)
 
 // Logic:
 // 1.  Traverse all boundaries:
 // 1.1 First Row, Last Row, First Column, Last Column
-// 2.  If there is 'O' and it is unvisted, then
+// 2.  If there is "1" and it is unvisted, then
 // 2.1 Do dfs/bfs from that node and mark all connected nodes as visited
 // 3.  After all 4 traversals have been done,
 // 3.1 Iterate through all rows and columns except above ones,
-// 3.2 If there is 'O' and unvisited, then replace it with 'X', else leave it as it is
+// 3.2 If there is "1" and unvisited, and increase the number of enclaves
 
 class Solution {
 public:
-	void doDfs(int n, int m, int sr, int sc, vector<vector<char>> &adj, vector<vector<int>> &visited) {
-		stack<pair<int, int>> s;
-		s.push({sr, sc});
+	void doBfs(int n, int m, int sr, int sc, vector<vector<int>> &adj, vector<vector<int>> &visited) {
+		queue<pair<int, int>> q;
+		q.push({sr, sc});
 
-		while(!s.empty()) {
-			int currRow = s.top().first;
-			int currCol = s.top().second;
-			s.pop();
+		while(!q.empty()) {
+			int currRow = q.front().first;
+			int currCol = q.front().second;
+			q.pop();
 
 			int delrow[4] = {-1, 1, 0, 0};
 			int delcol[4] = {0, 0, -1, 1};
@@ -43,41 +43,45 @@ public:
 				int ncol = currCol + delcol[i];
 
 				if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-				   adj[nrow][ncol]=='O' && !visited[nrow][ncol]) {
+				   adj[nrow][ncol]==1 && !visited[nrow][ncol]) {
 					visited[nrow][ncol] = 1;
-					s.push({nrow, ncol});
+					q.push({nrow, ncol});
 				}
 			}
 		}
 	}
 
-	void replaceOX(int n, int m, vector<vector<char>> &adj) {
+	int getEnclaves(int n, int m, vector<vector<int>> &adj) {
 		vector<vector<int>> visited(n, vector<int> (m, 0));
 
 		// outer columns
 		for(int i=0; i<n; i++) {
 			// first column
-			if(adj[i][0]=='O' && !visited[i][0]) doDfs(n, m, i, 0, adj, visited);
+			if(adj[i][0]==1 && !visited[i][0]) doBfs(n, m, i, 0, adj, visited);
 
 			// last column
-			if(adj[i][m-1]=='O' && !visited[i][m-1]) doDfs(n, m, i, m-1, adj, visited);
+			if(adj[i][m-1]==1 && !visited[i][m-1]) doBfs(n, m, i, m-1, adj, visited);
 		}
 
 		// outer rows
 		for(int j=0; j<m; j++) {
 			// first row
-			if(adj[0][j]=='O' && !visited[0][j]) doDfs(n, m, 0, j, adj, visited);
+			if(adj[0][j]==1 && !visited[0][j]) doBfs(n, m, 0, j, adj, visited);
 
 			// last row
-			if(adj[n-1][j]=='O' && !visited[n-1][j]) doDfs(n, m, n-1, j, adj, visited);
+			if(adj[n-1][j]==1 && !visited[n-1][j]) doBfs(n, m, n-1, j, adj, visited);
 		}
+
+		int enclaves = 0;
 
 		// inner rows and columns
 		for(int i=1; i<n-1; i++) {
 			for(int j=1; j<m-1; j++) {
-				if(adj[i][j]=='O' && !visited[i][j]) adj[i][j] = 'X';
+				if(adj[i][j]==1 && !visited[i][j]) enclaves++;
 			}
 		}
+
+		return enclaves;
 	}   
 };
 
@@ -90,7 +94,7 @@ int main() {
 	cin>>n>>m;
 
 	// adjaceny matrix
-	vector<vector<char>> adj(n, vector<char> (m, 0));
+	vector<vector<int>> adj(n, vector<int> (m, 0));
 
 	for(int i=0; i<n; i++) {
 		for(int j=0; j<m; j++) {
@@ -100,13 +104,9 @@ int main() {
 
 	Solution S;
 
-	S.replaceOX(n, m, adj);
+	int ans = S.getEnclaves(n, m, adj);
 
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<m; j++) {
-			cout<<adj[i][j]<<" ";
-		} cout<<"\n";
-	}
+	cout<<ans<<"\n";
 
 	return 0;
 }
