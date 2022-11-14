6d729c21e5b76cff1a2b12318fcf414740268caf
githubotoro <yupuday.git@gmail.com>
2022-11-14 23:55:49 +0530

    number-of-distinct-islands

---
+++ graphs/13-number-of-distinct-islands.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void stayTORO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif 
+}
+
+// DFS - Depth First Search
+// Principle - LIFO (Last In First Out)
+
+// Logic:
+// 1.  We will fo bfs/dfs traversal from all disjoint nodes
+// 2.  We will create a vector of pairs (row, column)
+// 2.1 While inserting element in vector of pairs,
+//     we will subract base row and base col each time
+// 2.2 Base's Row, Col = Row, Col of node from where we started the bfs/dfs traversal
+// 3.  We will push each bfs/dfs traversal in a map
+// 4.  In the end, size of the map will give us the number of distinct islands
+
+class Solution {
+public:
+	vector<pair<int, int>> doDfs(int sr, int sc, vector<vector<int>> &adj, vector<vector<int>> &visited) {
+		int n = adj.size();
+		int m = adj[0].size();
+
+		visited[sr][sc] = 1;
+		stack<pair<int, int>> s;
+		s.push({sr, sc});
+
+		pair<int, int> base = {sr, sc};
+
+		vector<pair<int, int>> dfs;
+		dfs.push_back({sr - base.first, sc - base.second});
+		
+		while(!s.empty()) {
+			int currRow = s.top().first;
+			int currCol = s.top().second;
+			s.pop();
+
+			int delrow[4] = {-1, 1, 0, 0};
+			int delcol[4] = {0, 0, -1, 1};
+
+			for(int i=0; i<4; i++) {
+				int nrow = currRow + delrow[i];
+				int ncol = currCol + delcol[i];
+
+				if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+				   adj[nrow][ncol]==1 && !visited[nrow][ncol]) {
+					visited[nrow][ncol] = 1;
+					dfs.push_back({nrow - base.first, ncol - base.second});
+					s.push({nrow, ncol});
+				}
+			}
+		}
+
+		return dfs;
+	}
+
+	int getDistinctIslands(vector<vector<int>> &adj) {
+		int n = adj.size();
+		int m = adj[0].size();
+
+		vector<vector<int>> visited(n, vector<int>(m, 0));
+
+		map<vector<pair<int, int>>, int> mp;
+
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				if(!visited[i][j] && adj[i][j]==1) {
+					vector<pair<int, int>> island = doDfs(i, j, adj, visited);
+					mp[island] = 1;
+				}
+			}
+		}
+
+		return mp.size();
+	}
+};
+
+int main() {
+	stayTORO();
+
+	// n - number of nodes
+	// m - number of edges
+	int n, m;
+	cin>>n>>m;
+
+	// adjaceny matrix
+	vector<vector<int>> adj(n, vector<int> (m, 0));
+
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<m; j++) {
+			cin>>adj[i][j];
+		}
+	}
+
+	Solution S;
+
+	int ans = S.getDistinctIslands(adj);
+
+	cout<<ans<<"\n";
+
+	return 0;
+}
+
+// == IN ==
+// 4 5
+// 1 1 0 0 0
+// 1 1 0 0 0
+// 0 0 0 1 1
+// 0 0 0 1 1
+
+// == OUT ==
+// 1
